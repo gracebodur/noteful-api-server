60cfc40c3f84affb2738f1bec566d0150d3b4014
Grace <mgracebodur@gmail.com>
2020-01-24 20:54:02 -0500

    Updates

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://Malek@localhost/noteful-api",
-  TEST_DB_URL: process.env.DB_URL || "postgresql://Malek@localhost/noteful-api"
+  DATABASE_URL: process.env.DATABASE_URL || "postgresql://Malek@localhost/noteful-api",
+  TEST_DATABASE_URL: process.env.DATABASE_URL || "postgresql://Malek@localhost/noteful-api"
 };
